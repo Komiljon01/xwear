0085028e9b5a7e98082626a254b51e6bfda93266
Komiljon <nortoshovk@gmail.com>
2024-08-13 23:47:40 +0500

    Added navbar

---
+++ src/js/navbar.js
@@ -0,0 +1,29 @@
+const navList = document.querySelector(".nav__lists"),
+  navOpen = document.querySelector(".nav-open"),
+  navClose = document.querySelector(".nav-close"),
+  overlay = document.querySelector(".overlay"),
+  body = document.body;
+
+const openMenu = () => {
+  navList.style = "transform: translateX(0)";
+  overlay.classList.remove("hidden");
+  overlay.classList.add("show");
+  body.style.overflowY = "hidden";
+};
+
+const closeMenu = () => {
+  navList.style = "transform: translateX(-100%)";
+  overlay.classList.remove("show");
+  overlay.classList.add("hidden");
+  body.style.overflowY = "auto";
+};
+
+navOpen.addEventListener("click", openMenu);
+
+navClose.addEventListener("click", closeMenu);
+
+overlay.addEventListener("click", () => {
+  if (!overlay.classList.contains("hidden")) {
+    closeMenu();
+  }
+});
