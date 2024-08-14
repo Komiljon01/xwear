9e5bddfddd76f93451376cffa665990dbb0cb09e
Komiljon <nortoshovk@gmail.com>
2024-08-14 22:54:32 +0500

    Added home page blogs section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".sneakers", {
+const swiperProducts = new Swiper(".sneakers", {
   slidesPerView: 1,
   spaceBetween: 15,
   loop: true,
@@ -21,6 +21,28 @@ const swiper = new Swiper(".sneakers", {
       spaceBetween: 30,
     },
     400: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+const swiperBlogs = new Swiper(".swiper__blogs", {
+  slidesPerView: 1,
+  spaceBetween: 31,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    900: {
+      slidesPerView: 3,
+    },
+    460: {
       slidesPerView: 2,
     },
   },
