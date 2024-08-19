27a424796739304b1195d7c54247781acaf58b44
Komiljon <nortoshovk@gmail.com>
2024-08-19 17:10:00 +0500

    Added calculate-cost page modal after filling out the manager form

--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,3 +1,4 @@
+// Calculate Tabs
 const calcBtnsParent = document.querySelector(".calculate__content-buttons");
 const calcBtns = calcBtnsParent.querySelectorAll("button");
 const tabsCalcContent = document.querySelectorAll(
@@ -36,3 +37,39 @@ calcBtnsParent.addEventListener("click", (e) => {
     });
   }
 });
+
+// Calculate Modal
+
+const calcOverlay = document.querySelector(".overlay__calculate"),
+  calcModal = document.querySelector(".calculate__modal"),
+  calcModalClose = document.querySelector(".calculate__modal-close"),
+  calcModalExit = document.querySelector(".calculate__modal-btn");
+
+const calcForm = document.querySelector(".manager-form");
+
+function openCalcModal() {
+  calcModal.classList.remove("hidden");
+  calcOverlay.classList.remove("hidden");
+  body.style.overflowY = "hidden";
+
+  if (!calcModal.classList.contains("hidden")) {
+    setTimeout(() => closeCalcModal(), 3000);
+  }
+}
+
+function closeCalcModal() {
+  calcModal.classList.add("hidden");
+  calcOverlay.classList.add("hidden");
+  body.style.overflowY = "visible";
+}
+
+calcForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  openCalcModal();
+  calcForm.reset();
+});
+
+calcModalClose.addEventListener("click", closeCalcModal);
+calcModalExit.addEventListener("click", closeCalcModal);
+calcOverlay.addEventListener("click", closeCalcModal);
